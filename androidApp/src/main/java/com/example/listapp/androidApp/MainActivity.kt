c487169a2744bfc5e6fcb17d38d8856b33d8be98
Hoang Luong <holuong@expedia.com>
2020-10-31 02:34:50 +1000

    work

--- androidApp/src/main/java/com/example/listapp/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/example/listapp/androidApp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.listapp.androidApp
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.listapp.shared.Greeting
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,9 +12,28 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var textField: TextView
+    lateinit var addButton: Button
+    lateinit var itemFragment: ItemFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textField = findViewById(R.id.textField)
+        textField.visibility = View.INVISIBLE
+        addButton = findViewById(R.id.addButon)
+
+        addButton.setOnClickListener {
+            addButtonClicked()
+        }
+    }
+
+    fun addButtonClicked() {
+        if (textField.text.isEmpty()) {
+            textField.visibility = View.VISIBLE
+        } else {
+
+        }
     }
 }
